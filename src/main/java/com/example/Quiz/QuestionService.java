1ff3e49cd24624dbb7f549b7942f448c416e63e5
JeTSkY1h <jetsky1h@googlemail.com>
2022-06-22 15:24:59 +0200

    added mongoatlas db

--- src/main/java/com/example/Quiz/QuestionService.java
+++ src/main/java/com/example/Quiz/QuestionService.java
@@ -1,6 +1,5 @@
 package com.example.Quiz;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,7 +14,6 @@ public class QuestionService {
     private final QuestionRepo questionRepo;
     
     List<Question> getQuestions(){
-        
         return questionRepo.findAll();
     }
 
